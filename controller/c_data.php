4db6c53391eaaa8144278201f4353a3598a3dd05
congdat.trinh <congdat.trinh@tanchonggroup.com>
2018-09-27 14:59:04 +0700

    update new version

--- controller/c_data.php
+++ controller/c_data.php
@@ -1,11 +1,30 @@
 <?php 
 
-    include('model/m_data.php');
+    require_once('model/m_data.php');
+
     class C_data{
         public function index(){
             $m_data = new M_data();
             $user = $m_data->getUser();
-            return array('user'=>$user);
+            return $user;
+        }
+
+        public function insertUser(){
+            $m_data = new M_data();
+            $result = $m_data->insertUser();
+            return $result;
+        }
+
+        public function deleteUser(){
+            $m_data = new M_data();
+            $result = $m_data->deleteUser();
+            return $result;
+        }
+
+        public function updateUser(){
+            $m_data = new M_data();
+            $result = $m_data->updateUser();
+            return $result;
         }
 
     }
--- model/m_data.php
+++ model/m_data.php
@@ -1,14 +1,22 @@
 <?php 
-    include('database.php');
+    require_once('database.php');
     class M_data extends database{
+
         public function getUser(){
+            $conn = $this->connect();
             $sql = "SELECT * FROM user";
-            $this->setQuery($sql);
-            return $this->loadAllRows();
-    
+            $result= $conn->query($sql);
+            $users = array();
+            if($result->num_rows >0){
+                while($user = mysqli_fetch_assoc($result)){
+                    $users[] = $user;
+                }
+            }
+            return $users;
         }
 
         public function insertUser(){
+            $conn = $this->connect();
             $user = $_POST['user'];
             $email = $_POST['email'];
             if(!$user || !$email){
@@ -27,6 +35,7 @@
         }
 
         public function deleteUser(){
+            $conn = $this->connect();
             $id = $_POST['id'];
             mysqli_query($conn,"DELETE FROM user Where id = '$id'");
             echo $result=1;
@@ -36,9 +45,21 @@
             $user = $_POST['user'];
             $email = $_POST['email'];
             $id = $_POST['id'];
-            mysqli_query($conn,"UPDATE user SET user='$user', gmail='$email' where id = '$id'");
-            echo $result = 1;
+            if(!$user || !$email ||!$id){
+                $result = 2;
+            }elseif (!strpos($email, "@") || !strpos($email, ".")) {
+                $result = 3;
+            }else {
+                $sql = "UPDATE user SET user='$user', gmail='$email' where id = '$id'";
+                    if ($conn->query($sql) === TRUE) {
+                        $result = 1; 
+                    }else{
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+            }
+            echo $result;
         }
+
     }
 
 ?>
